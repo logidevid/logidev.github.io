62becb695bcb8ee6c4609f810e987999f855f57f
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2022-08-22 14:14:14 +0800

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# logidev.github.io
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="App container">
+      <img src="/logidev-logo-white.svg"></img>
+      <h1>Boom Shakalaka!</h1>
+      <p>We're currently working on creating our website.</p>
+    </section>
+  );
+}
+
+export default App;
