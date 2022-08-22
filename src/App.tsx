e305ca49c466f5cba7c8d395f467a672d3bac764
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2022-08-22 20:13:06 +0800

    Implement responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,29 @@ import "./App.css";
 
 function App() {
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto sm:px-10 px-4">
       <div className="flex flex-col h-screen justify-center">
-        <img className="w-[150px] ml-[6px]" src="/logidev-logo-white.svg" />
+        <img
+          className="sm:w-[150px] w-[120px] ml-[6px]"
+          src="/logidev-logo-white.svg"
+        />
 
-        <div className="mt-[30px]">
-          <h1 className="main-title text-[72px] font-bold">Boom Shakalaka!</h1>
-          <p className="text-[22px]">
+        <div className="sm:mt-[30px] mt-[16px]">
+          <h1 className="main-title leading-tight md:text-[72px] sm:text-[62px] text-[46px] font-bold">
+            Boom Shakalaka!
+          </h1>
+          <p className="sm:text-[22px] text-[14px]">
             We're currently working on creating our website.
           </p>
         </div>
 
-        <div className="flex gap-4 mt-[42px]">
+        <div className="flex gap-4 sm:mt-[42px] mt-[24px]">
           <a
             className="hover:text-gray-400"
             href="mailto:logidevid@gmail.com"
             target="_blank"
           >
-            <AiOutlineMail size={32} />
+            <AiOutlineMail size={30} />
           </a>
 
           <a
@@ -32,7 +37,7 @@ function App() {
             href="https://github.com/logidevid"
             target="_blank"
           >
-            <AiOutlineGithub size={32} />
+            <AiOutlineGithub size={30} />
           </a>
 
           <a
@@ -40,7 +45,7 @@ function App() {
             href="https://wa.me/089606620616"
             target="_blank"
           >
-            <AiOutlineWhatsApp size={32} />
+            <AiOutlineWhatsApp size={30} />
           </a>
         </div>
       </div>
